91ec3ed3f0aa93d8599cc232686a03b7c18446a6
vtetris1 <145947319+vtetris1@users.noreply.github.com>
2023-12-03 20:06:03 -0800

    8th auto pixel test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/OpMode3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/OpMode3.java
@@ -26,9 +26,9 @@ public class OpMode3 extends LinearOpMode {
         // Put initialization blocks here.
         waitForStart();
         while (opModeIsActive()) {
-            double horizontal = gamepad1.left_stick_x * 0.5;
-            double vertical = -gamepad1.left_stick_y * 0.5;
-            double turn = gamepad1.right_stick_x * 0.5;
+            double horizontal = gamepad1.left_stick_x * 0.75;
+            double vertical = -gamepad1.left_stick_y * 0.75;
+            double turn = gamepad1.right_stick_x * 0.75;
 
             robot.setDrivePower(vertical+turn-horizontal,vertical-turn+horizontal,vertical+turn+horizontal,vertical-turn-horizontal);
 
@@ -113,40 +113,23 @@ public class OpMode3 extends LinearOpMode {
             if (gamepad2.left_trigger > 0.5) {
                 robot.grabServo.setPosition(0.4); // open
             }
-            else if (gamepad2.right_trigger>0.5){
+            else if (gamepad2.right_trigger > 0.5){
                 robot.grabServo.setPosition(1); // close
             }
 
-
+ 
 //tilt servo
             double initTilt = 0.0;
 
-            if (gamepad2.right_stick_y > 0.7) {
-                initTilt += 0.1;
-
-                if (initTilt > 1.0) {
-                    initTilt = 1.0;
-                    robot.tiltServo.setPosition(-initTilt);
-                }
-
-                else robot.tiltServo.setPosition(-initTilt);
+            if (gamepad2.right_stick_y > 0.7){
+                initTilt = 1.0;
+                robot.tiltServo.setPosition(initTilt);
             }
 
-            if (gamepad2.right_stick_y < -0.7) {
-                initTilt -= 0.1;
-
-                if (initTilt < -1.0) {
-                    initTilt = -1.0;
-                    robot.tiltServo.setPosition(initTilt);
-                }
-
-                else robot.tiltServo.setPosition(initTilt);
-
-
+            if (gamepad2.right_stick_y < - 0.7){
+                initTilt = 0.15;
+                robot.tiltServo.setPosition(initTilt);
             }
-
-
-
             telemetry.update();
 
 
