4fbb955c5e3fbe67ad6484ca3499a20544d215a1
vtetris1 <145947319+vtetris1@users.noreply.github.com>
2024-01-07 19:02:32 -0800

    13th worked on auto added emergency release fixed airplane launch angle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Blue_Near_v1_A2_B6.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Blue_Near_v1_A2_B6.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 //test
-
+//commit test
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
